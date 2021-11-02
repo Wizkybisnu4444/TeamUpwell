fe41dd68025f585fc1e6635be38eddc46a4cc617
alpha1532 <91642014+alpha1532@users.noreply.github.com>
2021-11-02 20:31:38 +1100

    build 1 minor fixes

--- GAD181_TeamDownwell_BalloonLament/Assets/scripts/playerMove.cs
+++ GAD181_TeamDownwell_BalloonLament/Assets/scripts/playerMove.cs
@@ -12,6 +12,8 @@ public class playerMove : MonoBehaviour
 
     public int thrustSpeed;
 
+    private bool _descend;
+
 
 
     // Update is called once per frame
@@ -25,6 +27,9 @@ public class playerMove : MonoBehaviour
 
         _thrusting = Input.GetKey(KeyCode.Tab);
 
+        _descend = Input.GetKey(KeyCode.DownArrow);
+        _descend = Input.GetKey(KeyCode.S);
+
     }
 
     void FixedUpdate()
@@ -34,6 +39,12 @@ public class playerMove : MonoBehaviour
         {
             _rb.AddForce(this.transform.up * this.thrustSpeed);
         }
+
+        if (_descend)
+        {
+            _rb.AddForce(this.transform.up * -10);
+        }
+
     }
 
 
