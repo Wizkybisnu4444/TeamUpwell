63e033ce79c8eaf14554ea6948afeefa93dda24b
Wizkybisnu4444 <78828818+Wizkybisnu4444@users.noreply.github.com>
2021-11-24 15:26:46 +1100

    stuff clashing, trying to fix

--- GAD181_TeamDownwell_BalloonLament/Assets/scripts/BulletBehaviour.cs
+++ GAD181_TeamDownwell_BalloonLament/Assets/scripts/BulletBehaviour.cs
@@ -7,11 +7,23 @@ public class BulletBehaviour : MonoBehaviour
     public GameObject bullet;
     public int bulletN = 0;
     public bool reload = false;
+    public GameObject i;
+    public GameObject ii;
+    public GameObject iii;
+    public GameObject iv;
+    public GameObject v;
+    public GameObject max;
 
     // Start is called before the first frame update
     void Start()
     {
         bulletN = 5;
+        i.SetActive(true);
+        ii.SetActive(true);
+        iii.SetActive(true);
+        iv.SetActive(true);
+        v.SetActive(true);
+        max.SetActive(true);
     }
 
     // Update is called once per frame
@@ -24,11 +36,55 @@ public class BulletBehaviour : MonoBehaviour
                 bulletN--;
                 GameObject ammo = Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
                 ammo.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * 50000);
+                if (bulletN == 4)
+                {
+                    max.SetActive(false);
+                    v.SetActive(false);
+                    i.SetActive(true);
+                    ii.SetActive(true);
+                    iii.SetActive(true);
+                    iv.SetActive(true);
+                }
+                else if (bulletN == 3)
+                {
+                    max.SetActive(false);
+                    v.SetActive(false);
+                    iv.SetActive(false);
+                    i.SetActive(true);
+                    ii.SetActive(true);
+                    iii.SetActive(true);
+
+                }
+                else if (bulletN == 2)
+                {
+                    max.SetActive(false);
+                    v.SetActive(false);
+                    iv.SetActive(false);
+                    iii.SetActive(false);
+                    i.SetActive(true);
+                    ii.SetActive(true);
+
+                }
+                else
+                {
+                    max.SetActive(false);
+                    v.SetActive(false);
+                    iv.SetActive(false);
+                    iii.SetActive(false);
+                    ii.SetActive(false);
+                    i.SetActive(true);
+                }
             }
         }
         if (bulletN < 1 && reload == false)
         {
             reload = true;
+            max.SetActive(false);
+            v.SetActive(false);
+            iv.SetActive(false);
+            iii.SetActive(false);
+            ii.SetActive(false);
+            i.SetActive(false);
             StartCoroutine(Reload());
         }
     }
@@ -36,6 +92,13 @@ public class BulletBehaviour : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
         bulletN = 5;
+        bulletN = 5;
+        i.SetActive(true);
+        ii.SetActive(true);
+        iii.SetActive(true);
+        iv.SetActive(true);
+        v.SetActive(true);
+        max.SetActive(true);
         reload = false;
     }
 }
