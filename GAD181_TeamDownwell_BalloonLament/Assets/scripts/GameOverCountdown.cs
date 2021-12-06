8df3d9b70e7a8229ea4db3d544011f821920f59f
WCM0 <91642091+WCM0@users.noreply.github.com>
2021-12-06 16:56:13 +1100

    Game Over Countdown Working

--- GAD181_TeamDownwell_BalloonLament/Assets/scripts/GameOverCountdown.cs
+++ GAD181_TeamDownwell_BalloonLament/Assets/scripts/GameOverCountdown.cs
@@ -6,33 +6,43 @@ using UnityEngine.SceneManagement;
 
 public class GameOverCountdown : MonoBehaviour
 {
-    public float timeLeft;
-    public Text countDisplay;
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public Text timeText;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        // Starts the timer automatically
+        timerIsRunning = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        timeLeft--;
-        if (timeLeft < 0)
+        if (timerIsRunning)
         {
-            GameOver();
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                SceneManager.LoadScene("TitleScreen");
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
         }
-
-
-
     }
 
-    private void GameOver()
+    void DisplayTime(float timeToDisplay)
     {
+        timeToDisplay += 1;
 
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{1:00}", minutes, seconds);
     }
 
 }
